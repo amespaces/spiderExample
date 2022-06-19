f94d8512ab0147b57e14e9f8125a109ee5760fa7
amespaces <amespaces@163.com>
2022-06-19 11:53:44 +0800

    修改了爬虫案例的代码

--- blog_spider.py
+++ blog_spider.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 urls = [
     f"https://www.cnblogs.com/#p{page}"
@@ -8,4 +9,16 @@ urls = [
 
 def craw(url):
     req = requests.get(url)
-    print(url, len(req.text))
+    return req.text
+
+
+def parse(html):
+    # class = "post-item-title"
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all("a", class_="post-item-title")
+    return [(link["href"], link.get_text()) for link in links]
+
+
+if __name__ == '__main__':
+    for res in parse(craw(urls[2])):
+        print(res)
